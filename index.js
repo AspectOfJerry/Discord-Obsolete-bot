7f29e4639a822dc03d4b6af27157c1f4d25c1ac8
Jerry <jerryfu_mc@outlook.com>
2022-11-08 18:41:37 -0500

    test

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const DiscordJS = require('discord.js')
-const {Intents} = DiscordJS
-require('dotenv').config()
+const DiscordJS = require('discord.js');
+const {Intents} = DiscordJS;
+require('dotenv').config();
 
 const client = new DiscordJS.Client({
     intents: [
         Intents.FLAGS.GUILDS
     ]
-})
+});
 
 client.on('ready', () => {
-    console.log('"JerryBot [Obsolete]#0182" is now online.')
+    console.log('"JerryBot [Obsolete]#0182" is now online.');
     setTimeout(() => {client.user.setStatus('dnd')}, 5000);
-    console.log("Updating the bot's status to 'dnd'.")
-})
+    console.log("Updating the bot's status to 'dnd'.");
+});
 
-client.login(process.env.DISCORD_BOT_TOKEN)
+client.login(process.env.DISCORD_BOT_TOKEN);
